a7bbd64314d9ae5b1d27db4a04b29b8db12d4e27
Victoria Celeste Vocos <victoria.vocos@claro.com.ar>
2023-07-10 15:38:21 -0300

    Primer commit

---
+++ productos/src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+export const ItemListContainer = (props) => {
+    return <>
+        <div className="Container">
+            <div className="text-center text-secondary">
+                <h3>{props.greeting}</h3>
+            </div>
+        </div>
+    </>
+
+}
